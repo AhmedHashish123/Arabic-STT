77866cab834c9be77ed4500de58b5e860fd0e641
Ahmed Hashish <33344853+AhmedHashish123@users.noreply.github.com>
2024-01-10 20:07:09 +0200

    updated metrics

--- src/metrics_calculator.py
+++ src/metrics_calculator.py
@@ -4,13 +4,15 @@ from models.enhanced_wav2vec.STTEnhancedWav2Vec import STTEnhancedWav2Vec
 from models.new_architecture.STTNewArchitecture import STTNewArchitecture
 from utils.metrics import calculate_wer
 
+whisper_tiny = STTWhisper("tiny")
+whisper_base = STTWhisper("base")
 whisper_small = STTWhisper("small")
 enhanced_wav2vec = STTEnhancedWav2Vec()
 new_architecture = STTNewArchitecture()
 
 vocabulary = ['ا', 'ب', 'ت', 'ث', 'ج', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'س', 'ش', 'ص', 'ض', 'ط', 'ظ', 'ع', 'غ', 'ف', 'ق', 'ك', 'ل', 'م', 'ن', 'ه', 'و', 'ي', 'ء', 'آ', 'أ', 'إ', 'ؤ', 'ئ', 'ة', 'ى', 'ﻻ', 'ﻷ', 'ﻹ', 'ﻵ',' ', '.']
 
-common_voice = load_dataset("mozilla-foundation/common_voice_13_0", "ar", split="train+validation+test")
+common_voice = load_dataset("mozilla-foundation/common_voice_13_0", "ar", split="test")
 
 common_voice = common_voice.remove_columns(["client_id", "audio", "up_votes", "down_votes", "age", "gender", "accent", "locale", "segment", "variant"])
 
@@ -21,4 +23,4 @@ def process_transcriptions(data_item):
 
 common_voice = common_voice.map(process_transcriptions)
 
-calculate_wer(common_voice, whisper_small, enhanced_wav2vec, new_architecture)
+calculate_wer(common_voice, whisper_tiny, whisper_base, whisper_small, enhanced_wav2vec, new_architecture)
--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -1,26 +1,39 @@
 from jiwer import wer
 
-def calculate_wer(common_voice, model_1, model_2, model_3):
+def calculate_wer(common_voice, model_1, model_2, model_3, model_4, model_5):
   total_wer_1 = 0
   total_wer_2 = 0
   total_wer_3 = 0
-
+  total_wer_4 = 0
+  total_wer_5 = 0
+  counter = 0
   for i in range(len(common_voice)):
     reference = common_voice[i]['sentence']
     hypothesis_1 = model_1.transcribe(common_voice[i]['path'])
     hypothesis_2 = model_2.transcribe(common_voice[i]['path'])
     hypothesis_3 = model_3.transcribe(common_voice[i]['path'])
+    hypothesis_4 = model_4.transcribe(common_voice[i]['path'])
+    hypothesis_5 = model_5.transcribe(common_voice[i]['path'])
     wer_1 = wer(reference, hypothesis_1)
     wer_2 = wer(reference, hypothesis_2)
     wer_3 = wer(reference, hypothesis_3)
+    wer_4 = wer(reference, hypothesis_4)
+    wer_5 = wer(reference, hypothesis_5)
     total_wer_1 += wer_1
     total_wer_2 += wer_2
     total_wer_3 += wer_3
+    total_wer_4 += wer_4
+    total_wer_5 += wer_5
+    counter += 1
 
   avg_wer_1 = total_wer_1 / len(common_voice)
   avg_wer_2 = total_wer_2 / len(common_voice)
   avg_wer_3 = total_wer_3 / len(common_voice)
+  avg_wer_4 = total_wer_4 / len(common_voice)
+  avg_wer_5 = total_wer_5 / len(common_voice)
 
   print(avg_wer_1)
   print(avg_wer_2)
   print(avg_wer_3)
+  print(avg_wer_4)
+  print(avg_wer_5)
