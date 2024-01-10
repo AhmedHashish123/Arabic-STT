c448ef92a5dafb5c36dd18d3908ebd42d0231512
Ahmed Hashish <33344853+AhmedHashish123@users.noreply.github.com>
2024-01-10 05:35:32 +0200

    added WER metric calculation

---
+++ src/metrics_calculator.py
@@ -0,0 +1,33 @@
+from datasets import load_dataset
+from models.whisper_fine_tuned.STTWhisper import STTWhisper
+from models.enhanced_wav2vec.STTEnhancedWav2Vec import STTEnhancedWav2Vec
+from models.new_architecture.STTNewArchitecture import STTNewArchitecture
+from utils.metrics import calculate_wer
+
+whisper_small = STTWhisper("tiny")
+enhanced_wav2vec = STTEnhancedWav2Vec()
+new_architecture = STTNewArchitecture()
+
+vocabulary = ['ا', 'ب', 'ت', 'ث', 'ج', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'س', 'ش', 'ص', 'ض', 'ط', 'ظ', 'ع', 'غ', 'ف', 'ق', 'ك', 'ل', 'م', 'ن', 'ه', 'و', 'ي', 'ء', 'آ', 'أ', 'إ', 'ؤ', 'ئ', 'ة', 'ى', 'ﻻ', 'ﻷ', 'ﻹ', 'ﻵ',' ', '.']
+
+fleurs_test = load_dataset("google/fleurs", "ar_eg", split="test")
+
+def update_audio_path_test(data_item):
+    parts = data_item['path'].split('\\')
+    parts.insert(-1, 'test')
+    data_item['path'] = '\\'.join(parts)
+    data_item['sentence'] = data_item['transcription']
+    return data_item
+
+fleurs_test = fleurs_test.map(update_audio_path_test)
+
+fleurs_test = fleurs_test.remove_columns(["id", "num_samples", "audio", "transcription", "raw_transcription", "gender", "lang_id", "language", "lang_group_id"])
+
+def process_transcriptions(data_item):
+  new_sentence = ''.join([char for char in data_item["sentence"] if char in vocabulary])
+  data_item["sentence"] = new_sentence
+  return data_item
+
+fleurs_test = fleurs_test.map(process_transcriptions)
+
+calculate_wer(fleurs_test, whisper_small, enhanced_wav2vec, new_architecture)
---
+++ src/utils/metrics.py
@@ -0,0 +1,32 @@
+from jiwer import wer
+
+def calculate_wer(fleurs_test, model_1, model_2, model_3):
+  total_wer_1 = 0
+  total_wer_2 = 0
+  total_wer_3 = 0
+
+  for i in range(len(fleurs_test)):
+    reference = fleurs_test[i]['sentence']
+    hypothesis_1 = model_1.transcribe(fleurs_test[i]['path'])
+    hypothesis_2 = model_2.transcribe(fleurs_test[i]['path'])
+    hypothesis_3 = model_3.transcribe(fleurs_test[i]['path'])
+    print(hypothesis_1)
+    print(hypothesis_2)
+    print(hypothesis_3)
+    wer_1 = wer(reference, hypothesis_1)
+    wer_2 = wer(reference, hypothesis_2)
+    wer_3 = wer(reference, hypothesis_3)
+    print(wer_1)
+    print(wer_2)
+    print(wer_3)
+    total_wer_1 += wer_1
+    total_wer_2 += wer_2
+    total_wer_3 += wer_3
+
+  avg_wer_1 = total_wer_1 / len(fleurs_test)
+  avg_wer_2 = total_wer_2 / len(fleurs_test)
+  avg_wer_3 = total_wer_3 / len(fleurs_test)
+
+  print(avg_wer_1)
+  print(avg_wer_2)
+  print(avg_wer_3)
