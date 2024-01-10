6774428e02d95ddbf4bb01d15ade96d8f58720e0
Ahmed Hashish <33344853+AhmedHashish123@users.noreply.github.com>
2024-01-10 05:49:13 +0200

    bug fixes

--- src/metrics_calculator.py
+++ src/metrics_calculator.py
@@ -10,16 +10,16 @@ new_architecture = STTNewArchitecture()
 
 vocabulary = ['ا', 'ب', 'ت', 'ث', 'ج', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'س', 'ش', 'ص', 'ض', 'ط', 'ظ', 'ع', 'غ', 'ف', 'ق', 'ك', 'ل', 'م', 'ن', 'ه', 'و', 'ي', 'ء', 'آ', 'أ', 'إ', 'ؤ', 'ئ', 'ة', 'ى', 'ﻻ', 'ﻷ', 'ﻹ', 'ﻵ',' ', '.']
 
-fleurs_val = load_dataset("google/fleurs", "ar_eg", split="val")
+fleurs_val = load_dataset("google/fleurs", "ar_eg", split="validation")
 
-def update_audio_path_test(data_item):
+def update_audio_path_val(data_item):
     parts = data_item['path'].split('\\')
-    parts.insert(-1, 'val')
+    parts.insert(-1, 'dev')
     data_item['path'] = '\\'.join(parts)
     data_item['sentence'] = data_item['transcription']
     return data_item
 
-fleurs_val = fleurs_val.map(update_audio_path_test)
+fleurs_val = fleurs_val.map(update_audio_path_val)
 
 fleurs_val = fleurs_val.remove_columns(["id", "num_samples", "audio", "transcription", "raw_transcription", "gender", "lang_id", "language", "lang_group_id"])
 
